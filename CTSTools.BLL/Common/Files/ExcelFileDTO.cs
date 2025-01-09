c6259bdded491a9ef81684db68a7b928969b8cb8
armando monrreal vela <armandomonrreal@hotmail.com>
2025-01-09 10:36:56 -0600

    Creation of classes in bll to import excel records (ExcelDataImport_Service, ExcelFileDTO and GenericCatalogDTO)

---
+++ CTSTools.BLL/Common/Files/ExcelFileDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTSTools.BLL.Common.Files
+{
+    public class ExcelFileDTO
+    {
+        public string ColumnName { get; set; }
+        public string HeaderName { get; set; }
+    }
+}
